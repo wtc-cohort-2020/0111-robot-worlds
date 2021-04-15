774dbbeaed0b05a5c552e6119d19eb4bdb71bd77
{bbotes} <{bbotes@student.wethinkcode.co.za}>
2021-04-15 09:58:52 +0200

    Client class from Nick

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -2,6 +2,7 @@ package Client;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.net.*;
@@ -14,7 +15,8 @@ import java.util.Scanner;
 
 public class Client {
     static String command;
-    static String[] arguments = new String[2];
+    static String[] arguments = new String[3];
+    static String name;
 
 
     public static void main(String[] args) {
@@ -23,46 +25,40 @@ public class Client {
                 PrintStream out = new PrintStream(socket.getOutputStream());
                 BufferedReader in = new BufferedReader(new InputStreamReader(
                         socket.getInputStream()))
-        )
-        {
+        ) {
             Scanner scanner = new Scanner(System.in);
-            String input;
-            System.out.println("Give me a name: ");
-            input = scanner.nextLine();
-            JsonObject myRobot = new JsonObject();
-            myRobot.addProperty("robot", input);
-            myRobot.addProperty("command", "launch");
-            myRobot.addProperty("arguments", "");
-            out.println(myRobot.toString());
-            out.flush();
-            String messageFromServer = in.readLine();
-            System.out.println("Response: "+messageFromServer);
-            while (true){
+
+
+//            String messageFromServer = in.readLine();
+//            System.out.println("Response: "+messageFromServer);
+            while (true) {
+                String input;
                 input = scanner.nextLine();
-                if(input.equalsIgnoreCase("quit")){
+                if (input.equalsIgnoreCase("quit")) {
                     break;
                 }
 
-                myRobot = new JsonObject();
-                myRobot.addProperty("robot", "Gert");
+                JsonObject myRobot = new JsonObject();
+
 
                 splitCommand(input);
-                System.out.println(command);
-                System.out.println(arguments[0]);
+//                System.out.println(name);
+//                System.out.println(command);
+                System.out.println(Arrays.toString(arguments));
 
+                Gson gson = new Gson();
+
+                myRobot.addProperty("name", name);
                 myRobot.addProperty("command", command);
+                String myArgs = gson.toJson(arguments);
+                myRobot.addProperty("arguments", myArgs);
 
-                if (arguments.length == 2) {
-                    myRobot.addProperty("arguments", arguments[0]);
-                }
-                if (arguments.length==1) {
-                    myRobot.addProperty("arguments", arguments[0]);
-                }
+
                 out.println(myRobot.toString());
                 out.flush();
 
-                messageFromServer = in.readLine();
-                System.out.println("Response: "+messageFromServer);
+                String messageFromServer = in.readLine();
+                System.out.println("Response: " + messageFromServer);
             }
 
         } catch (IOException e) {
@@ -77,12 +73,10 @@ public class Client {
     }
 
 
-
-
     public static void splitCommand(String input) {
-        String [] commandAndArgs = input.split(" ");
+        String[] commandAndArgs = input.split(" ");
 
-        switch(commandAndArgs.length) {
+        switch (commandAndArgs.length) {
             case 1:
                 command = commandAndArgs[0];
                 return;
@@ -95,13 +89,37 @@ public class Client {
 
 
             case 3:
-                command = commandAndArgs[0];
-                arguments[0] = commandAndArgs[1];
-                arguments[1] = commandAndArgs[2];
-                return;
-        }
-    }
 
+                command = commandAndArgs[0];
+                System.out.println(commandAndArgs[1]);
+                if (commandAndArgs[1].equals("pistol")) {
+                    arguments[0] = "pistol";
+                    arguments[1] = "3";
+                    arguments[2] = "4";
+                }
+
+
+                else if (commandAndArgs[1].equals("sniper")) {
+                    arguments[0] = "sniper";
+                    arguments[1] = "3";
+                    arguments[2] = "4";
+                }
+
+                else if (commandAndArgs[1].equals("standard")) {
+                    arguments[0] = "standard";
+                    arguments[1] = "3";
+                    arguments[2] = "3";
+                }
+
+                else {
+                        arguments[0] = "shouldBeAnError";
+                        arguments[1] = "3";
+                        arguments[2] = "3";
+                }
+                name = commandAndArgs[2];
+
+        }
+
+    }
 }
 
-//                Socket socket = new Socket("127.0.0.1", 1999);
